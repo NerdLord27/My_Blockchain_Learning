f96c03acb61e6f496ed54936ceaac40dcef7ddd5
Nathaniel MacKinnon <nate.mackinnon@icloud.com>
2026-04-15 14:48:58 -0400

    constructor setup

---
+++ Blockchain.js
@@ -0,0 +1,8 @@
+class Block {
+    constructor(timestamp, data, previousHash = '') {
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+}
